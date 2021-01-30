1f2295b8ff6356068c080cb033f8e74364197b4c
Cassandra Xia <cssndrx@gmail.com>
2021-01-30 17:57:05 -0500

    Commit before installing vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,35 @@
 <template>
   <div id="app">
-    <div id="nav">
+<!--    <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+-->
+
+    <router-view/> 
+
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+
+h2{
+  font-weight: 400;
 }
 
-#nav {
+
+
+#app {
+  font-family: 'Source Sans Pro', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: white;
+
+  height: 100vh;
+  padding: 32px;
+}
+
+/* #nav {
   padding: 30px;
 
   a {
@@ -28,5 +40,10 @@
       color: #42b983;
     }
   }
+} */
+
+body{
+  margin: 0;
+  background-color: black;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!--    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+
+  <h1>The Road to Ten Gigatons</h1>
+  <h2>Carbon Removal Scale Up Challenge</h2>
+
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
-  }
+    // HelloWorld
+  },
+  data(){
+  	return {
+  		forests: 0,
+  		soil: 0,
+  		dac: 0,
+  		beccs: 0,
+  	}
+  },
 }
 </script>
